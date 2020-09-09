a8f088a263dfa4e425281c17ddf1671003a462a4
Gaspy <mpgasbarro@gmail.com>
2020-09-09 17:04:39 -0400

    Added the Youtube react library and successfully have video populating

--- workingout-app/src/Components/Pectorals.js
+++ workingout-app/src/Components/Pectorals.js
@@ -5,41 +5,57 @@ import SingleWorkoutPage from './SingleWorkoutPage'
 
 class Pectorals extends Component {
 	render() {
+		// Mapping through API, for Pectoral exercise with a Beginner difficulty
 		let workoutInfo = this.props.workout.map((pec) => {
 			if (pec.muscleGroupOne === 'Pectorals') {
-                    if (pec.levelOfDifficulty === "Beginner") 
-                    return (
-                    <Link to={`/pectorals/${pec.exercise}`}>
-                    <li>{pec.exercise} - {pec.levelOfDifficulty} </li>
-                    </Link>
-                    );
-                }
-        });
-        let workoutInfo2 = this.props.workout.map((pec) => {
-					if (pec.muscleGroupOne === 'Pectorals') {
-						if (pec.levelOfDifficulty === 'Intermediate')
-							return <li>{pec.exercise} - {pec.levelOfDifficulty} </li>;
-					}
-                });
-        let workoutInfo3 = this.props.workout.map((pec) => {
-					if (pec.muscleGroupOne === 'Pectorals') {
-						if (pec.levelOfDifficulty === 'Advanced')
-							return <li>{pec.exercise} - {pec.levelOfDifficulty} </li>;
-					}
-				});
+				if (pec.levelOfDifficulty === 'Beginner')
+					return (
+						<Link to={`/pectorals/${pec.exercise}`}>
+							<li>
+								{pec.exercise} - {pec.levelOfDifficulty}{' '}
+							</li>
+						</Link>
+					);
+			}
+		});
+		// Mapping through API, for Pectoral exercise with a Intermediate difficulty
+		let workoutInfo2 = this.props.workout.map((pec) => {
+			if (pec.muscleGroupOne === 'Pectorals') {
+				if (pec.levelOfDifficulty === 'Intermediate')
+					return (
+                        <Link to={`/pectorals/${pec.exercise}`}> 
+						<li>
+							{pec.exercise} - {pec.levelOfDifficulty}{' '}
+						</li>
+                        </Link>
+					);
+			}
+		});
+		// Mapping through API, for Pectoral exercise with an Advanced difficulty
+		let workoutInfo3 = this.props.workout.map((pec) => {
+			if (pec.muscleGroupOne === 'Pectorals') {
+				if (pec.levelOfDifficulty === 'Advanced')
+					return (
+                        <Link to={`/pectorals/${pec.exercise}`}>
+						<li>
+							{pec.exercise} - {pec.levelOfDifficulty}{' '}
+						</li>
+                        </Link>
+					);
+			}
+		});
 
 		return (
 			<div>
-					<div className='beginner'>{workoutInfo} </div>
-					<div className='intermediate'>{workoutInfo2}</div>
-					<div className='advanced'>{workoutInfo3}</div>
-				
+				<div className='beginner'>{workoutInfo} </div>
+				<div className='intermediate'>{workoutInfo2}</div>
+				<div className='advanced'>{workoutInfo3}</div>
+
 				<Route
 					path='/pectorals/:workout'
 					exact
 					render={(routerProp) => {
-                        return <SingleWorkoutPage 
-                        match={routerProp.match} />;
+						return <SingleWorkoutPage match={routerProp.match} />;
 					}}
 				/>
 			</div>
--- workingout-app/src/Components/SingleWorkoutPage.js
+++ workingout-app/src/Components/SingleWorkoutPage.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
+import Youtube from 'react-youtube';
 
 let workoutTarget = '';
+let videoID = '';
 
 class SingleWorkoutPage extends Component {
+	_onReady = (event) => {
+		event.target.pauseVideo();
+	};
 	render() {
-		console.log(this.props.match.params.workout);
+		const opts = {
+			height: '740',
+			width: '940',
+		};
+		// Looping through the data, to make sure correct exercise is dynamically updated and matches with what was selected
 		for (let i = 0; i < this.props.workoutData.length; i++) {
 			if (
 				this.props.workoutData[i].exercise === this.props.match.params.workout
@@ -13,7 +22,21 @@ class SingleWorkoutPage extends Component {
 				workoutTarget = this.props.workoutData[i];
 			}
 		}
-		return <nav> {workoutTarget.exercise} </nav>;
+		//videoID equates to the id needed on the react Youtube
+		videoID = workoutTarget.workout_url.split('?v=')[1];
+		return (
+			<nav>
+				<Youtube opts={opts} onReady={this._onReady} videoId={videoID} />
+				{workoutTarget.exercise}
+				{workoutTarget.muscleGroupOne}
+				<img
+					src={workoutTarget.workout_img}
+					alt={workoutTarget.description}
+					width='500'
+					height='500'
+				/>
+			</nav>
+		);
 	}
 }
 
