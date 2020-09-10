f210ef8840e9402b69cd69029cff333fa4643c3c
Gaspy <mpgasbarro@gmail.com>
2020-09-10 12:02:51 -0400

    Have Bicep view page up and working. Still no stylizing on any aspect

--- workingout-app/src/App.js
+++ workingout-app/src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
+import SingleWorkoutPage from './Components/SingleWorkoutPage';
 import Pectorals from './Components/Pectorals';
 import Triceps from './Components/Triceps';
-import SingleWorkoutPage from './Components/SingleWorkoutPage';
 import Deltoids from './Components/Deltoids';
+import Biceps from './Components/Biceps';
 
 let workoutUrl =
 	'https://immense-earth-33685.herokuapp.com/workout/?format=json';
@@ -32,6 +33,7 @@ class App extends Component {
 				<nav>
 					<Header />
 				</nav>
+        <nav>  
 				<Link to='/pectorals'>
 					<img
 						src='https://st2.depositphotos.com/1909187/10981/i/950/depositphotos_109811754-stock-photo-chest-muscles-pectoralis-major-and.jpg'
@@ -39,16 +41,17 @@ class App extends Component {
 						width='150'
 						height='150'
 					/>
-				</Link>
+				  </Link>
+        </nav>
 				<main>
-					<Route
+				<Route
 						exact
 						path='/pectorals'
 						render={() => {
 							return <Pectorals workout={this.state.workouts} />;
 						}}
 					/>
-					<Route
+				<Route
 						exact
 						path='/pectorals/:workout'
 						render={(routerProp) => {
@@ -60,23 +63,26 @@ class App extends Component {
 							);
 						}}
 					/>
-					<Link to='/triceps'>
+          </main>
+          <nav> 
+				<Link to='/triceps'>
 						<img
 							src='https://www.peakptfitness.com/wp-content/uploads/2017/04/Fotolia_110740617_S-300x300.jpg'
 							alt='human body - Triceps'
 							width='150'
 							height='150'
 						/>
-					</Link>
+				</Link>
+          </nav>
 					<main>
-						<Route
+				<Route
 							exact
 							path='/triceps'
 							render={() => {
 								return <Triceps workout={this.state.workouts} />;
 							}}
 						/>
-						<Route
+				<Route
 							exact
 							path='/triceps/:workout'
 							render={(routerProp) => {
@@ -88,22 +94,26 @@ class App extends Component {
 								);
 							}}
 						/>
-						<Link to='/deltoids'>
+          </main>  
+          <nav> 
+				<Link to='/deltoids'>
 							<img
 								src='https://oldschoollabs.com/wp-content/uploads/2019/07/Deltoid-Muscle-Breakdown.jpg'
 								alt='human body - Deltoids'
 								width='150'
 								height='150'
 							/>
-						</Link>
-						<Route
+				</Link>
+          </nav>
+        <main> 
+				<Route
 							exact
 							path='/deltoids'
 							render={() => {
 								return <Deltoids workout={this.state.workouts} />;
 							}}
 						/>
-						<Route
+				<Route
 							exact
 							path='/deltoids/:workout'
 							render={(routerProp) => {
@@ -115,7 +125,37 @@ class App extends Component {
 								);
 							}}
 						/>
-					</main>
+        </main>   
+        <nav>  
+				<Link to='/biceps'>
+							<img
+								src='https://iaom-us.com/wp-content/uploads/2018/11/Fotolia_110740333_S-693x675.jpg'
+								alt='human body - biceps'
+								width='150'
+								height='150'
+							/>
+				</Link>
+        </nav> 
+        <main> 
+				<Route
+							exact
+							path='/biceps'
+							render={() => {
+								return <Biceps workout={this.state.workouts} />;
+							}}
+						/>
+				<Route
+							exact
+							path='/biceps/:workout'
+							render={(routerProp) => {
+								return (
+									<SingleWorkoutPage
+										match={routerProp.match}
+										workoutData={this.state.workouts}
+									/>
+								);
+							}}
+						/>
 				</main>
 			</div>
 		);
---
+++ workingout-app/src/Components/Biceps.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import SingleWorkoutPage from './SingleWorkoutPage';
+
+class Biceps extends Component {
+	render() {
+		// Mapping through API, for Biceps exercise with a Beginner difficulty
+		let workoutInfo = this.props.workout.map((bicep) => {
+			if (
+				bicep.muscleGroupOne === 'Biceps' &&
+				bicep.levelOfDifficulty === 'Beginner'
+			) {
+				return (
+					<Link to={`/biceps/${bicep.exercise}`}>
+						<li>
+							{bicep.exercise} - {bicep.levelOfDifficulty}{' '}
+						</li>
+					</Link>
+				);
+			}
+		});
+		// Mapping through API, for Biceps exercise with a Intermediate difficulty
+		let workoutInfo2 = this.props.workout.map((bicep) => {
+			if (
+				bicep.muscleGroupOne === 'Biceps' &&
+				bicep.levelOfDifficulty === 'Intermediate'
+			) {
+				return (
+					<Link to={`/biceps/${bicep.exercise}`}>
+						<li>
+							{bicep.exercise} - {bicep.levelOfDifficulty}{' '}
+						</li>
+					</Link>
+				);
+			}
+		});
+		// Mapping through API, for Biceps exercise with an Advanced difficulty
+		let workoutInfo3 = this.props.workout.map((bicep) => {
+			if (
+				bicep.muscleGroupOne === 'Biceps' &&
+				bicep.levelOfDifficulty === 'Advanced'
+			) {
+				return (
+					<Link to={`/biceps/${bicep.exercise}`}>
+						<li>
+							{bicep.exercise} - {bicep.levelOfDifficulty}{' '}
+						</li>
+					</Link>
+				);
+			}
+		});
+		return (
+			<div>
+				<div className='beginner'>{workoutInfo} </div>
+				<div className='intermediate'>{workoutInfo2}</div>
+				<div className='advanced'>{workoutInfo3}</div>
+
+				<Route
+					path='/biceps/:workout'
+					exact
+					render={(routerProp) => {
+						return <SingleWorkoutPage match={routerProp.match} />;
+					}}
+				/>
+			</div>
+		);
+	}
+}
+
+export default Biceps;
